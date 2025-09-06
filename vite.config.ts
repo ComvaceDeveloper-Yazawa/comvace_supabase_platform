9423928d7a422ee69cc1be539f560ec94467054a
20230111ryo <ryosscv08@gmail.com>
2025-09-06 23:23:34 +0900

    update valiables

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default defineConfig(({ mode }) => {
+  // mode に応じて .env ファイルを読み込む
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [vue()],
+    base: env.VITE_BASE_URL || "/", // ← .env で設定できる
+    resolve: {
+      alias: {
+        "@": path.resolve(process.cwd(), "src"),
+      },
+    },
+  };
+});
