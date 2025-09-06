3a74e9b26258adb023457bd42fcf1b5aeee54efb
20230111ryo <ryosscv08@gmail.com>
2025-09-07 00:48:51 +0900

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,25 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
+import router from "./router";
+
+// import("disable-devtool").then(({ default: DisableDevTool }) => {
+//   DisableDevTool({
+//     // options
+//     disableMenu: true,
+//     clearIntervalWhenDevOpenTrigger: false,
+//     ondevtoolopen: () => {
+//       document.documentElement.style.display = "none";
+//       document.body.innerHTML = "none";
+//       // window.location.href = document.referrer;
+//     },
+//     ondevtoolclose: () => {
+//       window.location.reload();
+//     },
+//   });
+// });
+
 const app = createApp(App);
 app.use(createPinia());
+app.use(router);
 app.mount("#app");
