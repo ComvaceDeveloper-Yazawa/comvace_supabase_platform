edf7a02af7ff117f8ebd82fa77d5d5d2b5be6c1f
20230111ryo <ryosscv08@gmail.com>
2025-09-18 11:56:31 +0900

    fix: button layoutを修正

--- src/components/parts/CommonButton.vue
+++ src/components/parts/CommonButton.vue
@@ -61,11 +61,43 @@ const props = defineProps<{
   margin: 0 auto;
   border: 1px solid #fff;
   background-color: #ddd;
+}
+
+//SP用スタイル
+.sp-prev {
+  border-radius: 50px;
+  padding: 10px 0;
+  width: 200px;
+  color: #fff;
+  border: 1px solid #fff;
+  background-color: #33362f;
+
+  &:hover {
+    background-color: #000;
+    border: 1px solid #000;
+  }
+}
+.sp-next {
+  border-radius: 50px;
+  padding: 10px 0;
+  width: 200px;
+  color: #33362f;
+  border: 1px solid #fff;
+  background-color: #fff;
 
   &:hover {
     color: #fff;
     background-color: #000;
     border: 1px solid #000;
   }
+}
+.sp-inquiry {
+  border-radius: 50px;
+  padding: 10px 0;
+  width: 200px;
+  color: rgb(0, 0, 0, 0.5);
+  margin: 0 auto;
+  border: 1px solid #fff;
+  background-color: #ddd;
 }
 </style>
